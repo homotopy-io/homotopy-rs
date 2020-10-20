54b931049081087ba043a2adbf86da73935a9277
Nick Hu <me@nickhu.co.uk>
2020-10-20 17:12:27 +0100

    use mozilla nixpkgs overlay for rustc (prebuilt binaries)

--- shell.nix
+++ shell.nix
@@ -1,10 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
 with pkgs;
 mkShell {
   buildInputs = [
     nodejs_latest
+    (moz_nixpkgs.latest.rustChannels.stable.rust.override {
+      targets = ["wasm32-unknown-unknown"];
+      extensions = ["rust-src"];
+    })
     gcc wabt wasm-bindgen-cli wasm-pack
-    rustc cargo clippy rustfmt rust-analyzer
+    cargo clippy rustfmt rust-analyzer
   ];
 }
 
