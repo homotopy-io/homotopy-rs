c1bfb89accb4b6dea4a3412422c87a791eff14e9
github-action-benchmark <github@users.noreply.github.com>
2021-04-28 23:09:06 +0000

    add Rust Benchmark (criterion) benchmark result for dcc5b35717d1625e303c77230d511b4c06b6b0e0

---
+++ dev/bench/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1619651346145,
+  "repoUrl": "https://github.com/homotopy-io/homotopy-rs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lukas@heidemann.me",
+            "name": "Lukas Heidemann",
+            "username": "zrho"
+          },
+          "committer": {
+            "email": "lukas@heidemann.me",
+            "name": "Lukas Heidemann",
+            "username": "zrho"
+          },
+          "distinct": true,
+          "id": "dcc5b35717d1625e303c77230d511b4c06b6b0e0",
+          "message": "Show state update time in console.",
+          "timestamp": "2021-04-29T00:59:55+02:00",
+          "tree_id": "ed5f835338d395e21ea2b2d2545f5b58e9aa54c7",
+          "url": "https://github.com/homotopy-io/homotopy-rs/commit/dcc5b35717d1625e303c77230d511b4c06b6b0e0"
+        },
+        "date": 1619651337278,
+        "tool": "criterion",
+        "benches": [
+          {
+            "name": "contract scalar/left",
+            "value": 16.489,
+            "range": "+/- 0.342",
+            "unit": "us"
+          },
+          {
+            "name": "contract scalar/right",
+            "value": 17.432,
+            "range": "+/- 0.443",
+            "unit": "us"
+          },
+          {
+            "name": "contract beads/contract",
+            "value": 119.49,
+            "range": "+/- 3.930",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 130.95,
+            "range": "+/- 3.720",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 131.8,
+            "range": "+/- 7.940",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 155.87,
+            "range": "+/- 5.690",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 109.99,
+            "range": "+/- 3.100",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 794.03,
+            "range": "+/- 71.010",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 3.3096,
+            "range": "+/- 0.252",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 10.379,
+            "range": "+/- 0.295",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 30.449,
+            "range": "+/- 0.614",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 93.492,
+            "range": "+/- 3.040",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 272.42,
+            "range": "+/- 8.560",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 703.19,
+            "range": "+/- 17.300",
+            "unit": "ms"
+          },
+          {
+            "name": "",
+            "value": 2.9664,
+            "range": "+/- 0.053",
+            "unit": "us"
+          },
+          {
+            "name": "",
+            "value": 35.084,
+            "range": "+/- 1.868",
+            "unit": "us"
+          }
+        ]
+      }
+    ]
+  }
+}
