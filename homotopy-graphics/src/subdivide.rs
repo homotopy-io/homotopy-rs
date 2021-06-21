f7d0ae25b0a70628c4615dceb5a1a67053c2aee3
Akvile1206 <akvile.val1206@gmail.com>
2021-06-21 10:27:08 +0100

    create dummy 3D architecture

---
+++ homotopy-graphics/src/subdivide.rs
@@ -0,0 +1,11 @@
+use homotopy_core::mesh::Mesh;
+
+/// Subdivide square mesh one time
+pub fn subdivide3(control_mesh: Mesh) -> Mesh {
+    return control_mesh;
+}
+
+/// Subdivide cubical mesh one time
+pub fn subdivide4(control_mesh: Mesh) -> Mesh {
+    return control_mesh;
+}
