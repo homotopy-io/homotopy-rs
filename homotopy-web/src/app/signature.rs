6d86db848a8a774175093b711bf15d8c97e8c049
Akvile1206 <akvile.val1206@gmail.com>
2021-07-01 17:41:25 +0100

    Clean up color picker code

--- homotopy-web/src/app/signature.rs
+++ homotopy-web/src/app/signature.rs
@@ -4,7 +4,18 @@ use crate::model::proof::{Action, Color, GeneratorEdit, GeneratorInfo, Signature
 use homotopy_core::Generator;
 use im::HashMap;
 use palette::Srgb;
+use web_sys::Element;
 use yew::prelude::*;
+
+const COLOR_RGB: [[[u8; 3]; 4]; 2] = [
+    [
+        [41, 128, 185],
+        [192, 57, 43],
+        [243, 156, 18],
+        [142, 68, 173],
+    ],
+    [[39, 174, 96], [241, 196, 15], [255, 255, 255], [0, 0, 0]],
+];
 
 #[derive(Clone, PartialEq, Properties)]
 pub struct Props {
@@ -15,6 +26,7 @@ pub struct Props {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Message {
     Done(Generator),
+    DoneColor(Generator),
     Edit(Generator),
     Color(Generator),
     Rename(Generator, String),
@@ -64,6 +76,15 @@ impl Component for SignatureView {
                 self.renames.retain(|g, _| g != &generator);
                 self.recolors.retain(|g, _| g != &generator);
                 self.editing.retain(|g| g != &generator);
+                self.coloring.retain(|g| g != &generator);
+            }
+            Message::DoneColor(generator) => {
+                let dispatch = &self.props.dispatch;
+                for (g, n) in self.recolors.iter() {
+                    if n != &self.props.signature[g].color {
+                        dispatch.emit(Action::EditGenerator(*g, GeneratorEdit::Recolor(n.clone())));
+                    }
+                }
                 self.coloring.retain(|g| g != &generator);
             }
             Message::Rename(generator, name) => {
@@ -123,48 +144,115 @@ impl Component for SignatureView {
 impl SignatureView {
     fn color_generator(&self, generator: Generator, info: &GeneratorInfo) -> Html {
         let dispatch = &self.props.dispatch;
+        let color_ref = NodeRef::default();
+        let color_clone = color_ref.clone();
+        let picker_ref = NodeRef::default();
+        let buttons = COLOR_RGB
+            .iter()
+            .map(|colors| {
+                let b = colors.iter().map(|c| {
+                    let (icon, picker) = (color_ref.clone(), picker_ref.clone());
+                    html! {
+                        <span style=format!("color:{}", Color(Srgb::new(c[0], c[1], c[2])))
+                            class="signature__generator-edit"
+                            onclick=self.link.callback(move |_| {
+                                let color = Color(Srgb::new(c[0], c[1], c[2]));
+                                let s = format!("background-color:{}; height:32px",color);
+                                icon.cast::<Element>().unwrap().set_attribute("style",&s).unwrap();
+                                let p = picker.cast::<Element>().unwrap();
+                                p.set_attribute("type", "button").unwrap();
+                                p.set_attribute("value", &color.to_string()).unwrap();
+                                p.set_attribute("type", "color").unwrap();
+                                Message::Recolor(generator, Color(Srgb::new(c[0], c[1], c[2])))
+                            })
+                        >
+                            <Icon name={"circle"} size={IconSize::Icon18} />
+                        </span>
+                    }
+                }
+            ).collect::<Html>();
+                html! {
+                    <li
+                        class="signature__generator"
+                        style="justify-content: center; background-color:#f1f1f1"
+                    >
+                    {b}
+                    </li>
+                }
+            })
+            .collect::<Html>();
 
         html! {
-            <li
-                class="signature__generator"
-            >
-                <span style={format!("color:{}", self.recolors.get(&generator).map_or(&info.color, |color| color))}
-                    class="signature__generator-edit"
-                    onclick=self.link.callback(move |_| Message::Color(generator))
+            <>
+                <li
+                    class="signature__generator"
                 >
-                    <Icon name={"done"} size={IconSize::Icon18} />
-                </span>
-                <input
-                    type="text"
-                    class="signature__generator-name-input"
-                    value={
-                        self.renames.get(&generator).map_or(&info.name, |name| name)
-                    }
-                    oninput=self.link.callback(move |e: InputData| {
-                        Message::Rename(generator, e.value)
-                    })
-                    onkeyup=Callback::from(move |e: KeyboardEvent| {
-                        e.stop_propagation();
-                    })
-                />
-                <span class="signature__generator-dimension">
-                    {info.diagram.dimension()}
-                </span>
-                <span
-                    class="signature__generator-edit"
-                    onclick=dispatch.reform(move |_| Action::RemoveGenerator(generator))
+                    <span
+                        style={format!("background-color:{}; height:32px", self.recolors.get(&generator).map_or(&info.color, |color| color))}
+                        ref=color_ref.clone()
+                    >
+                    <span style="color:black"
+                        class="signature__generator-edit"
+                        onclick=self.link.callback(move |_| Message::DoneColor(generator))
+                    >
+                        <Icon name={"done"} size={IconSize::Icon18} />
+                    </span>
+                    </span>
+                    <input
+                        type="text"
+                        class="signature__generator-name-input"
+                        value={
+                            self.renames.get(&generator).map_or(&info.name, |name| name)
+                        }
+                        oninput=self.link.callback(move |e: InputData| {
+                            Message::Rename(generator, e.value)
+                        })
+                        onkeyup=Callback::from(move |e: KeyboardEvent| {
+                            e.stop_propagation();
+                        })
+                    />
+                    <span class="signature__generator-dimension">
+                        {info.diagram.dimension()}
+                    </span>
+                    <span
+                        class="signature__generator-edit"
+                        onclick=dispatch.reform(move |_| Action::RemoveGenerator(generator))
+                    >
+                        <Icon name={"delete"} size={IconSize::Icon18} />
+                    </span>
+                    <span
+                        class="signature__generator-edit"
+                        onclick=self.link.callback(move |_| {
+                            Message::Done(generator)
+                        })
+                    >
+                        <Icon name={"done"} size={IconSize::Icon18} />
+                    </span>
+                </li>
+                {buttons}
+                <li
+                    class="signature__generator"
+                    style="background-color:#f1f1f1"
                 >
-                    <Icon name={"delete"} size={IconSize::Icon18} />
-                </span>
-                <span
-                    class="signature__generator-edit"
-                    onclick=self.link.callback(move |_| {
-                        Message::Done(generator)
-                    })
-                >
-                    <Icon name={"done"} size={IconSize::Icon18} />
-                </span>
-            </li>
+                    <input
+                        style="border:none"
+                        ref=picker_ref.clone()
+                        type="color"
+                        value= {
+                            self.recolors.get(&generator).map_or(&info.color, |color| color)
+                        }
+                        oninput=self.link.callback(move |e: InputData| {
+                            let str = e.value;
+                            let r = u8::from_str_radix(&str[1..3], 16).unwrap();
+                            let g = u8::from_str_radix(&str[3..5], 16).unwrap();
+                            let b = u8::from_str_radix(&str[5..], 16).unwrap();
+                            let s = format!("background-color:{}", str.as_str());
+                            color_clone.cast::<Element>().unwrap().set_attribute("style",&s).unwrap();
+                            Message::Recolor(generator, Color(Srgb::new(r, g, b)))
+                        })
+                    />
+                </li>
+            </>
         }
     }
 
@@ -181,21 +269,6 @@ impl SignatureView {
                 >
                     <Icon name={"palette"} size={IconSize::Icon18} />
                 </span>
-                <input
-                    type="color"
-                    value= {
-                        log::info!("value {:?}", info.color);
-                        log::info!("value {:?}", info.color.to_string());
-                        self.recolors.get(&generator).map_or(&info.color, |color| color)
-                    }
-                    oninput=self.link.callback(move |e: InputData| {
-                        let str = e.value;
-                        let r = u8::from_str_radix(&str[1..3], 16).unwrap();
-                        let g = u8::from_str_radix(&str[3..5], 16).unwrap();
-                        let b = u8::from_str_radix(&str[5..], 16).unwrap();
-                        Message::Recolor(generator, Color(Srgb::new(r, g, b)))
-                    })
-                />
                 <input
                     type="text"
                     class="signature__generator-name-input"
