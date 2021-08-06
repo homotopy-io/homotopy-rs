61da3b5e474f7785e9315cb94379efed935c85d6
Nick Hu <me@nickhu.co.uk>
2021-08-06 13:43:32 +0100

    Add model shim for debugging via homotopy-web

---
+++ homotopy-web/tests/action.rs
@@ -0,0 +1,16 @@
+use homotopy_web::model::Action::*;
+use homotopy_web::model::*;
+
+#[test]
+#[ignore]
+fn action() {
+    let data: SerializedData = std::env::var("HOMOTOPY_IMPORT")
+        .map_or(Err(futures::io::ErrorKind::NotFound.into()), |fp| {
+            std::fs::read(fp)
+        })
+        .unwrap_or_default()
+        .into();
+    let mut state: State = Default::default();
+    let _ = state.update(ImportProof(data));
+    let _ = state.update(unimplemented!("trigger action goes here"));
+}
