6cebed670c7001885c5abe45c10f51a261af75be
calintat <calintat@gmail.com>
2021-08-25 15:36:40 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@f86661acd7ce6b47d09216d53d41ed7de2c5c1ab 🚀

--- index-6afa789199468434.js
+++ index-6afa789199468434.js
@@ -284,7 +284,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-54a2594530d238a0_bg.wasm', import.meta.url);
+        input = new URL('index-6afa789199468434_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
