283f96cf388baf3a2b5dbca01746297c49422da5
NickHu <me@nickhu.co.uk>
2021-08-27 12:02:08 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@5dad7e4584fa7bf665e1f956e84604c9a26437cc 🚀

--- index-86996ace85d8dc0b.js
+++ index-86996ace85d8dc0b.js
@@ -284,7 +284,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-6bd64bf756f714c3_bg.wasm', import.meta.url);
+        input = new URL('index-86996ace85d8dc0b_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
