667b2e42590dcf2fe7ed68b3a82c4e028ff1d7bd
Nathan Corbyn <me@nathancorbyn.com>
2021-09-06 13:13:49 +0100

    Fix projection

--- homotopy-web/src/app/renderers/diagram3d.rs
+++ homotopy-web/src/app/renderers/diagram3d.rs
@@ -1,21 +1,23 @@
-use ultraviolet::projection::lh_yup::perspective_gl;
+use ultraviolet::projection::rh_yup::perspective_gl;
+use ultraviolet::{Mat4, Vec3};
 
 use homotopy_graphics::gl::array::VertexArray;
 use homotopy_graphics::gl::frame::Frame;
 use homotopy_graphics::gl::{GlCtx, Result};
 use homotopy_graphics::{draw, program, vertex_array};
 
+use homotopy_graphics::clay;
+
 use crate::components::gl::Renderer;
 
 pub struct Diagram3D {
     mesh: VertexArray,
+    t: f32,
 }
 
 impl Renderer for Diagram3D {
     fn init(ctx: &mut GlCtx) -> Result<Self> {
-        let (elements, vertices) = homotopy_graphics::clay::examples::example_3()
-            .into()
-            .buffer(ctx)?;
+        let (elements, vertices) = clay::examples::example_3().into().buffer(ctx)?;
         let program = program!(
             ctx,
             "../../../glsl/vert.glsl",
@@ -25,13 +27,20 @@ impl Renderer for Diagram3D {
         )?;
         let mesh = vertex_array!(&program, &elements, { position: &vertices })?;
 
-        Ok(Self { mesh })
+        Ok(Self { mesh, t: 0.0 })
     }
 
-    fn update(&mut self, _dt: f32) {}
+    fn update(&mut self, dt: f32) {
+        self.t += 0.001 * dt;
+        while self.t >= std::f32::consts::TAU {
+            self.t -= std::f32::consts::TAU;
+        }
+    }
 
     fn render<'a>(&'a self, mut frame: Frame<'a>) {
-        let mvp = perspective_gl(30.0, 1.0, 0.5, 10.0);
+        let mvp = perspective_gl(f32::to_radians(30.0), 1.0, 0.5, 10.0)
+            * Mat4::from_translation(Vec3::new(0.0, 0.0, -7.0))
+            * Mat4::from_rotation_y(self.t);
 
         frame.draw(draw! {
             &self.mesh,
