1ea94743baebc07b39d7faa50631e1dedec0add3
calintat <calintat@gmail.com>
2021-09-17 11:30:33 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@03088dca54cdd3f3d639d5530754367ce362e33a 🚀

--- index-862a9f013d5e207.js
+++ index-862a9f013d5e207.js
@@ -329,7 +329,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b0c411ea0d3ea06f_bg.wasm', import.meta.url);
+        input = new URL('index-862a9f013d5e207_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -1010,8 +1010,8 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 526, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2896 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 959, __wbg_adapter_37);
+    imports.wbg.__wbindgen_closure_wrapper2880 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 945, __wbg_adapter_37);
         return addHeapObject(ret);
     };
 
