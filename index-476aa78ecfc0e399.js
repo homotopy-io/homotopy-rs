797746be69803bab66cdfb48f7aa7428680ec4a7
calintat <calintat@gmail.com>
2021-09-17 09:24:29 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@c552d29ac209405d34b541b93d8e91e04a8ee460 🚀

--- index-476aa78ecfc0e399.js
+++ index-476aa78ecfc0e399.js
@@ -329,7 +329,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-41a302ce6b524128_bg.wasm', import.meta.url);
+        input = new URL('index-476aa78ecfc0e399_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
