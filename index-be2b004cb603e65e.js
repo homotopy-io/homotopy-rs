43455790bdac12ded67770886e95dc307c59a882
doctorn <me@nathancorbyn.com>
2021-09-26 20:11:04 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@5402d4d1ac6bec05685dd91324ca60abc422822a 🚀

--- index-be2b004cb603e65e.js
+++ index-be2b004cb603e65e.js
@@ -329,7 +329,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-a18993c35185ef86_bg.wasm', import.meta.url);
+        input = new URL('index-be2b004cb603e65e_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
