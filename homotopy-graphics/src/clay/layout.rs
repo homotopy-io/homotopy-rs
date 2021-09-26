d14d145f1cf5be7f4d59d4caa6c6e421535e13e3
calintat <calintat@gmail.com>
2021-09-26 12:01:09 +0100

    Use correct cubicalisation depth

--- homotopy-graphics/src/clay/layout.rs
+++ homotopy-graphics/src/clay/layout.rs
@@ -44,12 +44,12 @@ pub struct MeshExtractor {
 }
 
 impl MeshExtractor {
-    pub fn new(diagram: &DiagramN, cubicalisation_depth: u8) -> Result<Self, DimensionError> {
+    pub fn new(diagram: &DiagramN, view_dimension: u8) -> Result<Self, DimensionError> {
         let diagram = Diagram::from(diagram.clone());
-        let graph = diagram.clone().cubicalise(&[Bias::Left].repeat(cmp::min(
-            cubicalisation_depth as usize,
-            diagram.dimension().saturating_sub(1),
-        )))?;
+        let graph =
+            diagram.clone().cubicalise(&[Bias::Left].repeat(
+                cmp::min(view_dimension as usize, diagram.dimension()).saturating_sub(1),
+            ))?;
 
         let mut mesh = Mesh::new(diagram.clone());
         let mut coords = HashMap::new();
