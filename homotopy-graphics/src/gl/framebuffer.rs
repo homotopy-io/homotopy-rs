2e61552ca0590038483f9640cfdcb7c7fefa32f9
Nathan Corbyn <me@nathancorbyn.com>
2022-01-05 18:24:11 +0000

    Add renderbuffer support

--- homotopy-graphics/src/gl/framebuffer.rs
+++ homotopy-graphics/src/gl/framebuffer.rs
@@ -2,20 +2,134 @@ use web_sys::{WebGl2RenderingContext, WebGlFramebuffer};
 
 use super::{GlCtx, GlCtxHandle, GlError, Result};
 
+#[derive(Copy, Clone)]
+enum AttachmentPoint {
+    Color(usize),
+    Depth,
+}
+
+impl AttachmentPoint {
+    const COLOR_ATTACHMENTS: [u32; 16] = [
+        WebGl2RenderingContext::COLOR_ATTACHMENT0,
+        WebGl2RenderingContext::COLOR_ATTACHMENT1,
+        WebGl2RenderingContext::COLOR_ATTACHMENT2,
+        WebGl2RenderingContext::COLOR_ATTACHMENT3,
+        WebGl2RenderingContext::COLOR_ATTACHMENT4,
+        WebGl2RenderingContext::COLOR_ATTACHMENT5,
+        WebGl2RenderingContext::COLOR_ATTACHMENT6,
+        WebGl2RenderingContext::COLOR_ATTACHMENT7,
+        WebGl2RenderingContext::COLOR_ATTACHMENT8,
+        WebGl2RenderingContext::COLOR_ATTACHMENT9,
+        WebGl2RenderingContext::COLOR_ATTACHMENT10,
+        WebGl2RenderingContext::COLOR_ATTACHMENT11,
+        WebGl2RenderingContext::COLOR_ATTACHMENT12,
+        WebGl2RenderingContext::COLOR_ATTACHMENT13,
+        WebGl2RenderingContext::COLOR_ATTACHMENT14,
+        WebGl2RenderingContext::COLOR_ATTACHMENT15,
+    ];
+
+    fn into_gl_const(self) -> u32 {
+        match self {
+            Self::Color(i) => Self::COLOR_ATTACHMENTS[i],
+            Self::Depth => WebGl2RenderingContext::DEPTH_ATTACHMENT,
+        }
+    }
+}
+
+pub trait Attachable {
+    // SAFETY: attached objects must not be dropped before the bound framebuffer
+    // is dropped
+    unsafe fn attach(&self, gl: &WebGl2RenderingContext, target: u32);
+}
+
+pub struct Attachment {
+    attachable: Box<dyn Attachable>,
+    point: AttachmentPoint,
+}
+
+impl Attachment {
+    fn new<T>(t: T, point: AttachmentPoint) -> Self
+    where
+        T: Attachable + 'static,
+    {
+        Self {
+            attachable: Box::new(t) as Box<dyn Attachable>,
+            point,
+        }
+    }
+
+    #[inline]
+    pub fn color<T>(t: T, id: usize) -> Self
+    where
+        T: Attachable + 'static,
+    {
+        Self::new(t, AttachmentPoint::Color(id))
+    }
+
+    #[inline]
+    pub fn depth<T>(t: T) -> Self
+    where
+        T: Attachable + 'static,
+    {
+        Self::new(t, AttachmentPoint::Depth)
+    }
+}
+
 pub struct Framebuffer {
     ctx: GlCtxHandle,
+    attachments: Vec<Attachment>,
     webgl_framebuffer: WebGlFramebuffer,
 }
 
 impl Framebuffer {
-    fn alloc(ctx: &GlCtx) -> Result<Self> {
+    fn alloc(ctx: &GlCtx, attachments: Vec<Attachment>) -> Result<Self> {
         let webgl_framebuffer = ctx
             .with_gl(WebGl2RenderingContext::create_framebuffer)
             .ok_or(GlError::Allocate)?;
 
-        Ok(Self {
+        let framebuffer = Self {
             ctx: ctx.ctx_handle(),
+            attachments,
             webgl_framebuffer,
+        };
+
+        let completeness_check = framebuffer.bind(|| {
+            ctx.with_gl(|gl| {
+                for attachment in &framebuffer.attachments {
+                    // SAFETY: we've already taken ownership of all of these objects,
+                    // so there is no way that they can be deallocated before the
+                    // framebuffer is dropped
+                    unsafe {
+                        attachment
+                            .attachable
+                            .attach(gl, attachment.point.into_gl_const())
+                    }
+                }
+
+                gl.check_framebuffer_status(WebGl2RenderingContext::FRAMEBUFFER)
+            })
+        });
+
+        if completeness_check == WebGl2RenderingContext::FRAMEBUFFER_COMPLETE {
+            Ok(framebuffer)
+        } else {
+            Err(GlError::Framebuffer)
+        }
+    }
+
+    #[inline]
+    pub(super) fn bind<F, U>(&self, f: F) -> U
+    where
+        F: FnOnce() -> U,
+    {
+        self.ctx.with_gl(|gl| {
+            gl.bind_framebuffer(
+                WebGl2RenderingContext::FRAMEBUFFER,
+                Some(&self.webgl_framebuffer),
+            );
+            let result = f();
+            gl.bind_framebuffer(WebGl2RenderingContext::FRAMEBUFFER, None);
+            result
         })
     }
 }
@@ -30,7 +144,7 @@ impl Drop for Framebuffer {
 
 impl GlCtx {
     #[inline]
-    pub fn mk_framebuffer(&self) -> Result<Framebuffer> {
-        Framebuffer::alloc(self)
+    pub fn mk_framebuffer(&self, attachments: Vec<Attachment>) -> Result<Framebuffer> {
+        Framebuffer::alloc(self, attachments)
     }
 }
---
+++ homotopy-graphics/src/gl/renderbuffer.rs
@@ -0,0 +1,90 @@
+use web_sys::{WebGl2RenderingContext, WebGlRenderbuffer};
+
+use super::{framebuffer::Attachable, GlCtx, GlCtxHandle, GlError, Result};
+
+pub struct Renderbuffer {
+    ctx: GlCtxHandle,
+    webgl_renderbuffer: WebGlRenderbuffer,
+}
+
+#[derive(Default)]
+pub struct RenderbufferOpts {
+    pub width: Option<u32>,
+    pub height: Option<u32>,
+}
+
+impl Renderbuffer {
+    fn alloc(ctx: &GlCtx, opts: RenderbufferOpts) -> Result<Self> {
+        let webgl_renderbuffer = ctx
+            .with_gl(|gl| gl.create_renderbuffer())
+            .ok_or(GlError::Allocate)?;
+
+        let renderbuffer = Self {
+            ctx: ctx.ctx_handle(),
+            webgl_renderbuffer,
+        };
+
+        renderbuffer.bind(|| {
+            ctx.with_gl(|gl| {
+                // NOTE could pass format as a renderbuffer option
+                gl.renderbuffer_storage(
+                    WebGl2RenderingContext::RENDERBUFFER,
+                    WebGl2RenderingContext::DEPTH_COMPONENT16,
+                    // FIXME(@doctorn) this needs to be adaptive
+                    opts.width.unwrap_or_else(|| ctx.width()) as i32,
+                    opts.height.unwrap_or_else(|| ctx.height()) as i32,
+                );
+            })
+        });
+
+        Ok(renderbuffer)
+    }
+
+    #[inline]
+    pub(super) fn bind<F, U>(&self, f: F) -> U
+    where
+        F: FnOnce() -> U,
+    {
+        self.ctx.with_gl(|gl| {
+            gl.bind_renderbuffer(
+                WebGl2RenderingContext::RENDERBUFFER,
+                Some(&self.webgl_renderbuffer),
+            );
+            let result = f();
+            gl.bind_renderbuffer(WebGl2RenderingContext::RENDERBUFFER, None);
+            result
+        })
+    }
+}
+
+impl Attachable for Renderbuffer {
+    #[inline]
+    unsafe fn attach(&self, gl: &WebGl2RenderingContext, target: u32) {
+        gl.framebuffer_renderbuffer(
+            WebGl2RenderingContext::FRAMEBUFFER,
+            target,
+            WebGl2RenderingContext::RENDERBUFFER,
+            Some(&self.webgl_renderbuffer),
+        );
+    }
+}
+
+impl Drop for Renderbuffer {
+    #[inline]
+    fn drop(&mut self) {
+        self.ctx
+            .with_gl(|gl| gl.delete_renderbuffer(Some(&self.webgl_renderbuffer)));
+    }
+}
+
+impl GlCtx {
+    #[inline]
+    pub fn mk_renderbuffer_with_opts(&self, opts: RenderbufferOpts) -> Result<Renderbuffer> {
+        Renderbuffer::alloc(self, opts)
+    }
+
+    #[inline]
+    pub fn mk_renderbuffer(&self) -> Result<Renderbuffer> {
+        self.mk_renderbuffer_with_opts(Default::default())
+    }
+}
