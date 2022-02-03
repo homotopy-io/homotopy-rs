f2bef337eba6ff1c5c98e1f974d4ad2a1ef4b103
NickHu <me@nickhu.co.uk>
2022-02-03 09:49:23 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@44a18a60a2c130abe786594f77287799dd4c4afc 🚀

--- homotopy_web.js
+++ homotopy_web.js
@@ -1083,7 +1083,7 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 354, __wbg_adapter_36);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3027 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3019 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 1084, __wbg_adapter_39);
         return addHeapObject(ret);
     };
