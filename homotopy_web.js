ad1a175dee160a9738cdc54060b735f680a5a431
calintat <calintat@gmail.com>
2022-02-03 19:42:04 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@a586ca1605dafc5f6d1aec62fffeb09963114ca0 🚀

--- homotopy_web.js
+++ homotopy_web.js
@@ -1063,28 +1063,28 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1087 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1098 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 354, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1088 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1099 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 354, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1090 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1101 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 354, __wbg_adapter_30);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1092 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1103 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 354, __wbg_adapter_33);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1095 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1106 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 354, __wbg_adapter_36);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3037 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 1086, __wbg_adapter_39);
+    imports.wbg.__wbindgen_closure_wrapper3052 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 1085, __wbg_adapter_39);
         return addHeapObject(ret);
     };
 
