159bdfb2a3b27380c44603b8698dc69ca2e4db37
Nick Hu <me@nickhu.co.uk>
2022-03-08 11:23:22 +0000

    Improve nix flake in preparation for CI overhaul [skip ci]

--- flake.nix
+++ flake.nix
@@ -2,43 +2,117 @@
   description = "homotopy.io rust edition";
 
   inputs = {
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    # last version before dream2nix
+    nix-cargo-integration.url = "github:yusdacra/nix-cargo-integration?rev=7fe944f24f1a7014b58ddafbdc8cf1ffae4de1ab";
+    # switch to upstream after both of these get merged:
+    # https://github.com/nix-community/naersk/pull/167
+    # https://github.com/nix-community/naersk/pull/227
+    naersk.url = "github:NickHu/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
+  outputs = inputs: let
+    outputs = inputs.nix-cargo-integration.lib.makeOutputs {
+      root = ./.;
+      buildPlatform = "crate2nix";
+      overrides = {
+        build = self: super: {
+          # crate2nix insta compatibility fix
+          testPreRun = ''
+            export INSTA_WORKSPACE_ROOT=$(pwd)
+            for file in $INSTA_WORKSPACE_ROOT/tests/snapshots/*; do
+              case $(basename $file) in (tests_*) continue;; esac;
+              mv $file $INSTA_WORKSPACE_ROOT/tests/snapshots/tests_$(basename $file)
+            done
+          '';
+        };
+        shell = self: super: {
+          packages =
+            super.packages
+            ++ (with self.pkgs; [
+              (lib.hiPrio rust-bin.nightly.latest.rustfmt)
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ]);
+          commands =
+            super.commands
+            ++ (with self.pkgs; [
+              {package = cargo-make;}
+              {package = devserver;}
+              {package = gdb;}
+              {package = rust-analyzer;}
+              {package = wasm-bindgen-cli;}
+            ]);
+        };
+      };
+    };
+  in
+    outputs
+    // inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import inputs.rust-overlay)];
+        pkgs = import inputs.nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in rec {
+        apps = {
+          lint = {
+            type = "app";
+            program = let
+              lint = pkgs.writeShellApplication {
+                name = "lint";
+                runtimeInputs = [pkgs.rust-bin.nightly.latest.rustfmt rust];
+                text = ''
+                  cargo fmt --version
+                  cargo fmt --all -- --check
+                  cargo clippy -- -D warnings
+                '';
+              };
+            in "${lint}/bin/lint";
           };
-          nativeBuildInputs = with pkgs; [
-            rust-bin.nightly.latest.rustfmt
-            (
-              rust-bin.stable.latest.minimal.override {
-                targets = [ "wasm32-unknown-unknown" ];
-                extensions = [ "rust-docs" "rust-src" "clippy" ];
-              }
-            )
-            wasm-bindgen-cli
-            wasm-pack
-          ];
+        };
+        defaultApp = {
+          type = "app";
+          program = toString (pkgs.writeShellScript
+          "homotopy-web"
+          ''
+            ${pkgs.devserver}/bin/devserver --path ${defaultPackage} --header Cross-Origin-Opener-Policy=same-origin --header Cross-Origin-Embedder-Policy=require-corp
+          '');
+        };
+        defaultPackage = let
+          rust = pkgs.rust-bin.stable.latest.minimal.override {
+            targets = ["wasm32-unknown-unknown"];
+          };
+          naersk = inputs.naersk.lib."${system}".override {
+            cargo = rust;
+            rustc = rust;
+          };
+          meta = builtins.fromTOML (builtins.readFile ./homotopy-web/Cargo.toml);
         in
-          {
-            devShell = with pkgs; mkShell {
-              buildInputs = nativeBuildInputs ++ [
-                cargo-make
-                devserver
-                rust-analyzer
-              ];
-              RUST_SRC_PATH = "${rust-bin.stable.latest.rust-src}/lib/rustlib/src/rust/library";
+          naersk.buildPackage {
+            inherit (meta.package) name version;
+            root = ./.;
+            copyLibs = true;
+            cargoBuildOptions = opts: opts ++ ["-p" "homotopy-web"];
+            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+            overrideMain = oldAttrs: {
+              nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with pkgs; [wasm-bindgen-cli]);
+              postBuild = ''
+                ${pkgs.wasm-bindgen-cli}/bin/wasm-bindgen --out-dir $out --no-typescript --target web target/wasm32-unknown-unknown/release/homotopy_web.wasm
+              '';
+              installPhase = ''
+                runHook preInstall
+                cp -r homotopy-web/static/* $out/
+                runHook postInstall
+              '';
             };
-          }
+          };
+      }
     );
 }
