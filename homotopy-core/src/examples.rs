3c634730cb6c1aac285e5f67369024c8a31c4324
Calin Tataru <calintat@gmail.com>
2022-03-30 16:29:31 +0100

    Add more handcoded examples

--- homotopy-core/src/examples.rs
+++ homotopy-core/src/examples.rs
@@ -181,3 +181,167 @@ pub fn bead_series(n: usize) -> (impl Signature, DiagramN) {
 
     (sig, res)
 }
+
+//    |
+//    m
+//   / \
+//  |   |
+//  |   u
+//  |
+//  m
+// / \
+pub fn monoid_unit() -> (impl Signature, DiagramN) {
+    let mut sig = SignatureBuilder::new();
+
+    // 0-cells
+    let x = sig.add_zero();
+
+    // 1-cells
+    let f = sig.add(x.clone(), x.clone()).unwrap();
+    let ff = f.attach(&f, Boundary::Target, &[]).unwrap();
+
+    // 2-cells
+    let m = sig.add(ff, f.clone()).unwrap();
+    let u = sig.add(x.identity(), f).unwrap();
+
+    (
+        sig,
+        m.attach(&u, Boundary::Source, &[1])
+            .unwrap()
+            .attach(&m, Boundary::Source, &[])
+            .unwrap(),
+    )
+}
+
+// |   |
+// |   b
+// | s |
+// a   |
+// |   |
+#[allow(clippy::many_single_char_names)]
+pub fn scalar_and_beads() -> (impl Signature, DiagramN) {
+    let mut sig = SignatureBuilder::new();
+
+    // 0-cells
+    let x = sig.add_zero();
+
+    // 1-cells
+    let f = sig.add(x.clone(), x.clone()).unwrap();
+
+    // 2-cells
+    let a = sig.add(f.clone(), f.clone()).unwrap();
+    let b = sig.add(f.clone(), f.clone()).unwrap();
+    let s = sig.add(x.identity(), x.identity()).unwrap();
+
+    (
+        sig,
+        s.attach(&f, Boundary::Source, &[])
+            .unwrap()
+            .attach(&f, Boundary::Target, &[])
+            .unwrap()
+            .attach(&a, Boundary::Source, &[0])
+            .unwrap()
+            .attach(&b, Boundary::Target, &[1])
+            .unwrap(),
+    )
+}
+
+pub fn snake() -> (impl Signature, DiagramN) {
+    let mut sig = SignatureBuilder::new();
+
+    // 0-cells
+    let x = sig.add_zero();
+
+    // 1-cells
+    let f = sig.add(x.clone(), x.clone()).unwrap();
+    let ff = f.attach(&f, Boundary::Target, &[]).unwrap();
+
+    // 2-cells
+    let cap = sig.add(ff.clone(), x.identity()).unwrap();
+    let cup = sig.add(x.identity(), ff).unwrap();
+    let snake = cap
+        .attach(&f, Boundary::Target, &[])
+        .unwrap()
+        .attach(&cup, Boundary::Source, &[1])
+        .unwrap();
+
+    // 3-cells
+    let snake_cancel = sig.add(snake, f.identity()).unwrap();
+
+    (sig, snake_cancel)
+}
+
+pub fn lips() -> (impl Signature, DiagramN) {
+    let mut sig = SignatureBuilder::new();
+
+    // 0-cells
+    let x = sig.add_zero();
+
+    // 1-cells
+    let f = sig.add(x.clone(), x.clone()).unwrap();
+    let ff = f.attach(&f, Boundary::Target, &[]).unwrap();
+
+    // 2-cells
+    let cap = sig.add(ff.clone(), x.identity()).unwrap();
+    let cup = sig.add(x.identity(), ff).unwrap();
+    let snake = cap
+        .attach(&f, Boundary::Target, &[])
+        .unwrap()
+        .attach(&cup, Boundary::Source, &[1])
+        .unwrap();
+
+    // 3-cells
+    let snake_death = sig.add(snake.clone(), f.identity()).unwrap();
+    let snake_birth = sig.add(f.identity(), snake).unwrap();
+
+    // 4-cells
+    let lips = sig
+        .add(
+            f.identity().identity(),
+            snake_birth
+                .attach(&snake_death, Boundary::Target, &[])
+                .unwrap(),
+        )
+        .unwrap();
+
+    (sig, lips)
+}
+
+pub fn pants_unit() -> (impl Signature, DiagramN) {
+    let mut sig = SignatureBuilder::new();
+
+    // 0-cells
+    let x = sig.add_zero();
+
+    // 1-cells
+    let f = sig.add(x.clone(), x.clone()).unwrap();
+    let ff = f.attach(&f, Boundary::Target, &[]).unwrap();
+
+    // 2-cells
+    let cap = sig.add(ff.clone(), x.identity()).unwrap();
+    let cup = sig.add(x.identity(), ff.clone()).unwrap();
+
+    // 3-cells
+    let saddle = sig
+        .add(
+            cap.attach(&cup, Boundary::Target, &[]).unwrap(),
+            ff.identity(),
+        )
+        .unwrap();
+    let sphere_birth = sig
+        .add(
+            x.identity().identity(),
+            cup.attach(&cap, Boundary::Target, &[]).unwrap(),
+        )
+        .unwrap();
+    let three_snake = saddle
+        .attach(&cap, Boundary::Target, &[])
+        .unwrap()
+        .attach(&sphere_birth, Boundary::Source, &[1])
+        .unwrap();
+
+    // 4-cells
+    let pants_unit = sig.add(three_snake, cap.identity()).unwrap();
+
+    (sig, pants_unit)
+}
