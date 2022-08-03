97c191d226093aced5920a0e9d48ce8e9e5d44ef
thud <thud@thud.dev>
2022-08-03 15:39:32 +0100

    Migrate to switch style inputs over checkboxes

--- homotopy-web/src/app/signature/item/preference.rs
+++ homotopy-web/src/app/signature/item/preference.rs
@@ -1,9 +1,12 @@
+use homotopy_graphics::style::Color;
 use yew::prelude::*;
 
 #[derive(Properties, Debug, Clone, PartialEq)]
 pub struct GeneratorPreferenceCheckboxProps {
-    pub name: &'static str,
+    pub left: &'static str,
+    pub right: &'static str,
     pub tooltip: Option<&'static str>,
+    pub color: Color,
     pub checked: bool,
     pub onclick: Callback<MouseEvent>,
     #[prop_or(false)]
@@ -21,14 +24,29 @@ impl Component for GeneratorPreferenceCheckbox {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let color = ctx.props().color.hex();
+
+        let border_style = format!("border: 1px solid {};", color);
+
+        let slider_style = format!(
+            "transform: translateX({}); background-color: {};",
+            if ctx.props().checked { "100%" } else { "0" },
+            color
+        );
+
         html! {
             <div
                 class={"signature__generator-preference"}
                 onclick={ctx.props().onclick.clone()}
+                style={border_style}
             >
-                <span>{ctx.props().name}</span>
+                <div class="signature__generator-preference-options-wrapper">
+                    <div class="signature__generator-preference-option">{ctx.props().left}</div>
+                    <div class="signature__generator-preference-option">{ctx.props().right}</div>
+                </div>
+                <div class="signature__generator-preference-slider" style={slider_style} />
                 <input
-                    type={"checkbox"}
+                    type="checkbox"
                     checked={ctx.props().checked}
                     disabled={ctx.props().disabled}
                 />
