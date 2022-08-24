0e1ebdc849fac75108c6e235ce31d5e5b68acb44
Yulong Huang <yh419@cam.ac.uk>
2022-08-24 05:49:23 -0700

    setup

---
+++ homotopy-web/src/app/image_export.rs
@@ -0,0 +1,75 @@
+use yew::prelude::*;
+
+use crate::model::proof::{Action, AttachOption, Signature};
+
+#[derive(Debug, Clone, PartialEq, Properties)]
+pub struct Props {
+    pub dispatch: Callback<Action>,
+    pub options: im::Vector<AttachOption>,
+    pub signature: Signature,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {}
+
+pub struct ImageExportView;
+
+impl Component for ImageExportView {
+    type Message = Message;
+    type Properties = Props;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            { for ctx.props().options.iter().map(|option| Self::view_option(ctx, option)) }
+        }
+    }
+}
+
+impl ImageExportView {
+    pub fn view_option(ctx: &Context<Self>, option: &AttachOption) -> Html {
+        let info = ctx
+            .props()
+            .signature
+            .generator_info(option.generator)
+            .unwrap();
+
+        let onclick = ctx.props().dispatch.reform({
+            let option = option.clone();
+            move |_| Action::Attach(option.clone())
+        });
+
+        let onmouseenter = ctx.props().dispatch.reform({
+            let option = option.clone();
+            move |_| Action::HighlightAttachment(Some(option.clone()))
+        });
+
+        let onmouseleave = ctx
+            .props()
+            .dispatch
+            .reform(|_| Action::HighlightAttachment(None));
+
+        // TODO: Maybe extract a common component for this and the signature.
+        // TODO: How should highlights work on touch devices?
+        html! {
+            <li
+                class="attach__option"
+                onclick={onclick}
+                onmouseenter={onmouseenter}
+                onmouseleave={onmouseleave}
+            >
+                <span
+                    class="attach__option-color"
+                    style={format!("background: {}", info.color)}
+                />
+            </li>
+        }
+    }
+}
--- homotopy-web/src/model.rs
+++ homotopy-web/src/model.rs
@@ -17,10 +17,11 @@ pub enum Action {
     History(history::Action),
     ImportProof(SerializedData),
     ExportProof,
-    ExportTikz,
-    ExportSvg,
-    ExportManim,
-    ExportStl,
+    ExportImage,
+    // ExportTikz,
+    // ExportSvg,
+    // ExportManim,
+    // ExportStl,
 }
 
 impl Action {
@@ -30,14 +31,14 @@ impl Action {
 
         match self {
             Self::Proof(action) => proof.is_valid(action),
-            Self::ExportTikz | Self::ExportSvg | Self::ExportManim => proof
+            Self::ExportImage => proof
                 .workspace
                 .as_ref()
                 .map_or(false, |ws| ws.view.dimension() == 2),
-            Self::ExportStl => proof
-                .workspace
-                .as_ref()
-                .map_or(false, |ws| ws.view.dimension() == 3),
+            // Self::ExportStl => proof
+            //     .workspace
+            //     .as_ref()
+            //     .map_or(false, |ws| ws.view.dimension() == 3),
             Self::History(history::Action::Move(dir)) => match dir {
                 history::Direction::Linear(Forward) => proof.can_redo(),
                 history::Direction::Linear(Backward) => proof.can_undo(),
@@ -115,75 +116,75 @@ impl State {
                 };
             }
 
-            Action::ExportTikz => {
-                let signature = self.with_proof(|p| p.signature.clone());
-                let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
-                let stylesheet = tikz::stylesheet(&signature);
-                let data = tikz::render(&diagram, &stylesheet, &signature).unwrap();
-                serialize::generate_download("homotopy_io_export", "tikz", data.as_bytes())
-                    .map_err(ModelError::Export)?;
-            }
+            // Action::ExportTikz => {
+            //     let signature = self.with_proof(|p| p.signature.clone());
+            //     let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
+            //     let stylesheet = tikz::stylesheet(&signature);
+            //     let data = tikz::render(&diagram, &stylesheet, &signature).unwrap();
+            //     serialize::generate_download("homotopy_io_export", "tikz", data.as_bytes())
+            //         .map_err(ModelError::Export)?;
+            // }
 
-            Action::ExportSvg => {
-                let signature = self.with_proof(|p| p.signature.clone());
+            // Action::ExportSvg => {
+            //     let signature = self.with_proof(|p| p.signature.clone());
 
-                // First we locate the element containing the SVG rendered the SVG rendering
-                // pipeline. We *could* do this by using a lookup by class name, which would not
-                // require any chances to components/panzoom.rs, but get_elements_by_class_name
-                // has a return type that requires a feature of web-sys that is not
-                // currently activated. Thus this solution avoids increases in build times.
-                let svg_element = web_sys::window()
-                    .expect("no window")
-                    .document()
-                    .expect("no document")
-                    .get_element_by_id("panzoom__inner__0")
-                    .expect("no SVG in document");
-                let svg = svg_element.inner_html();
+            //     // First we locate the element containing the SVG rendered the SVG rendering
+            //     // pipeline. We *could* do this by using a lookup by class name, which would not
+            //     // require any chances to components/panzoom.rs, but get_elements_by_class_name
+            //     // has a return type that requires a feature of web-sys that is not
+            //     // currently activated. Thus this solution avoids increases in build times.
+            //     let svg_element = web_sys::window()
+            //         .expect("no window")
+            //         .document()
+            //         .expect("no document")
+            //         .get_element_by_id("panzoom__inner__0")
+            //         .expect("no SVG in document");
+            //     let svg = svg_element.inner_html();
 
-                // We must now pull all the relevant stylesheets that are needed in the SVG.
-                // Failure to do so gives a fully-black SVG. We can generate the stylesheet in the
-                // same way the `SignatureStylesheet` struct does.
-                // We also strip the styles of whitespace since it is unneeded.
-                let stylesheet = {
-                    let mut inner_stylesheet = svg::stylesheet(&signature);
-                    inner_stylesheet.retain(|c| !c.is_whitespace());
-                    format!("<style>{}</style>", inner_stylesheet)
-                };
+            //     // We must now pull all the relevant stylesheets that are needed in the SVG.
+            //     // Failure to do so gives a fully-black SVG. We can generate the stylesheet in the
+            //     // same way the `SignatureStylesheet` struct does.
+            //     // We also strip the styles of whitespace since it is unneeded.
+            //     let stylesheet = {
+            //         let mut inner_stylesheet = svg::stylesheet(&signature);
+            //         inner_stylesheet.retain(|c| !c.is_whitespace());
+            //         format!("<style>{}</style>", inner_stylesheet)
+            //     };
 
-                // So we now have the SVG and its stylesheet in separate strings.
-                // It is not enough to just concatenate them, the stylesheets need to be inside the
-                // root element. Since we know that this will have form
-                // <svg.*><.*
-                // We just look for the first >< and use some very light indexing maths to insert
-                // the stylesheet in the appropriate place.
-                // Again, the function would be half as long and twice as clean if the SVG and the SVG's
-                // stylesheet were in the same place in the DOM.
-                let content_start = svg.find("><").unwrap() + 1;
-                let mut data = String::new();
-                data.push_str(&svg[..content_start]);
-                data.push_str(&stylesheet);
-                data.push_str(&svg[content_start..]);
+            //     // So we now have the SVG and its stylesheet in separate strings.
+            //     // It is not enough to just concatenate them, the stylesheets need to be inside the
+            //     // root element. Since we know that this will have form
+            //     // <svg.*><.*
+            //     // We just look for the first >< and use some very light indexing maths to insert
+            //     // the stylesheet in the appropriate place.
+            //     // Again, the function would be half as long and twice as clean if the SVG and the SVG's
+            //     // stylesheet were in the same place in the DOM.
+            //     let content_start = svg.find("><").unwrap() + 1;
+            //     let mut data = String::new();
+            //     data.push_str(&svg[..content_start]);
+            //     data.push_str(&stylesheet);
+            //     data.push_str(&svg[content_start..]);
 
-                serialize::generate_download("homotopy_io_export", "svg", data.as_bytes())
-                    .map_err(ModelError::Export)?;
-            }
+            //     serialize::generate_download("homotopy_io_export", "svg", data.as_bytes())
+            //         .map_err(ModelError::Export)?;
+            // }
 
-            Action::ExportManim => {
-                let signature = self.with_proof(|p| p.signature.clone());
-                let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
-                let stylesheet = manim::stylesheet(&signature);
-                let data = manim::render(&diagram, &signature, &stylesheet).unwrap();
-                serialize::generate_download("homotopy_io_export", "py", data.as_bytes())
-                    .map_err(ModelError::Export)?;
-            }
+            // Action::ExportManim => {
+            //     let signature = self.with_proof(|p| p.signature.clone());
+            //     let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
+            //     let stylesheet = manim::stylesheet(&signature);
+            //     let data = manim::render(&diagram, &signature, &stylesheet).unwrap();
+            //     serialize::generate_download("homotopy_io_export", "py", data.as_bytes())
+            //         .map_err(ModelError::Export)?;
+            // }
 
-            Action::ExportStl => {
-                let signature = self.with_proof(|p| p.signature.clone());
-                let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
-                let data = stl::render(&diagram, &signature).unwrap();
-                serialize::generate_download("homotopy_io_export", "stl", data.as_bytes())
-                    .map_err(ModelError::Export)?;
-            }
+            // Action::ExportStl => {
+            //     let signature = self.with_proof(|p| p.signature.clone());
+            //     let diagram = self.with_proof(|p| p.workspace.as_ref().unwrap().visible_diagram());
+            //     let data = stl::render(&diagram, &signature).unwrap();
+            //     serialize::generate_download("homotopy_io_export", "stl", data.as_bytes())
+            //         .map_err(ModelError::Export)?;
+            // }
 
             Action::ExportProof => {
                 let data = serialize::serialize(
@@ -216,6 +217,10 @@ impl State {
                 proof.signature = signature;
                 proof.workspace = workspace;
                 self.history.add(proof::Action::Imported, proof);
+            }
+
+            Action::ExportImage => {
+
             }
         }
 
