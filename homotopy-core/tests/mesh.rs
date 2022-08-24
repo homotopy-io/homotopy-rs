01e02d7130e57148b5c0f34710dc26b7f4d35c2a
Calin Tataru <calintat@gmail.com>
2022-08-24 23:38:45 +0100

    Fix mesh tests

--- homotopy-core/tests/mesh.rs
+++ homotopy-core/tests/mesh.rs
@@ -6,7 +6,11 @@ fn associator() {
     let (_, diagram) = examples::associator();
 
     let mesh = Mesh::<3>::new(&diagram.into()).unwrap();
-    let cubes = mesh.cubes().map(|cube| cube.points).collect::<Vec<_>>();
+    let cubes = mesh
+        .cubes()
+        .filter(|cube| cube.visible)
+        .map(|cube| cube.points)
+        .collect::<Vec<_>>();
 
     assert_debug_snapshot!(cubes);
 }
@@ -16,7 +20,11 @@ fn snake() {
     let (_, diagram) = examples::snake();
 
     let mesh = Mesh::<3>::new(&diagram.into()).unwrap();
-    let cubes = mesh.cubes().map(|cube| cube.points).collect::<Vec<_>>();
+    let cubes = mesh
+        .cubes()
+        .filter(|cube| cube.visible)
+        .map(|cube| cube.points)
+        .collect::<Vec<_>>();
 
     assert_debug_snapshot!(cubes);
 }
@@ -26,7 +34,11 @@ fn lips() {
     let (_, diagram) = examples::lips();
 
     let mesh = Mesh::<4>::new(&diagram.into()).unwrap();
-    let cubes = mesh.cubes().map(|cube| cube.points).collect::<Vec<_>>();
+    let cubes = mesh
+        .cubes()
+        .filter(|cube| cube.visible)
+        .map(|cube| cube.points)
+        .collect::<Vec<_>>();
 
     assert_debug_snapshot!(cubes);
 }
@@ -36,7 +48,11 @@ fn pants_unit() {
     let (_, diagram) = examples::pants_unit();
 
     let mesh = Mesh::<4>::new(&diagram.into()).unwrap();
-    let cubes = mesh.cubes().map(|cube| cube.points).collect::<Vec<_>>();
+    let cubes = mesh
+        .cubes()
+        .filter(|cube| cube.visible)
+        .map(|cube| cube.points)
+        .collect::<Vec<_>>();
 
     assert_debug_snapshot!(cubes);
 }
