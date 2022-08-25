50b239b84be516d97081372ca2ea40a9659f57bb
regular-citizen <7887982+regular-citizen@users.noreply.github.com>
2022-08-25 22:32:37 +0100

    Manually removed trivial crashes from fuzzer corpus.

--- homotopy-cli/fuzz/fuzz_targets/actions.rs
+++ homotopy-cli/fuzz/fuzz_targets/actions.rs
@@ -7,6 +7,11 @@ pub use homotopy_model::{history, migration, proof, proof::Action, serialize};
 libfuzzer_sys::fuzz_target!(|actions: Vec<Action>| {
     let mut proof: Proof = Default::default();
     for a in actions.iter() {
+        match a {
+            Action::EditSignature(_) => break,
+            Action::SelectPoints(_) => break,
+            _ => {}
+        }
         match proof.update(a) {
             Ok(_) => continue,
             Err(_) => break
