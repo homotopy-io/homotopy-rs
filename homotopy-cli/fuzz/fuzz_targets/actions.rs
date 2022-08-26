1e74be0e36a676f563da6ed5f4863d28321cff78
regular-citizen <7887982+regular-citizen@users.noreply.github.com>
2022-08-26 22:55:20 +0100

    Prevent libfuzzer from OOM with too many TakeIdentities.

--- homotopy-cli/fuzz/fuzz_targets/actions.rs
+++ homotopy-cli/fuzz/fuzz_targets/actions.rs
@@ -1,4 +1,3 @@
-
 #![no_main]
 
 pub use history::Proof;
@@ -7,11 +6,20 @@ pub use homotopy_model::{history, migration, proof, proof::Action, serialize};
 libfuzzer_sys::fuzz_target!(|actions: Vec<Action>| {
     let mut proof: Proof = Default::default();
     for a in actions.iter() {
+        // Don't go crazy-dimensional
+        if proof
+            .workspace
+            .as_ref()
+            .map(|w| w.visible_diagram().dimension() > 10)
+            .unwrap_or_default()
+        {
+            break;
+        }
         if proof.is_valid(a) {
-        match proof.update(a) {
-            Ok(_) => continue,
-            Err(_) => break
-        };
+            match proof.update(a) {
+                Ok(_) => continue,
+                Err(_) => break,
+            };
         } else {
             break;
         }
