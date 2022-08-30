1d973f53d1aff533631ade00f2d8cf977e5367b1
Chiara Sarti <7887982+regular-citizen@users.noreply.github.com>
2022-08-30 10:35:41 +0100

    New controls for full braid Beziers (#758)
    * Added example of braid contraction.
    
    * Changed controls for full braids (#473).
    
    * New full braid controls.
    
    * Revert commit 7a23cbab504d3552acc6bd2f6e626d5d30ba3707.

--- homotopy-model/tests/actions.rs
+++ homotopy-model/tests/actions.rs
@@ -2,14 +2,8 @@ use homotopy_core::typecheck::{typecheck, Mode};
 pub use homotopy_model::model::{history::Proof, proof::Action, serialize};
 use insta::*;
 
-// Example of how to write tests using action dumps.
-#[test]
-fn construct_associator() {
-    // Mind the beginning r#" and "#!
-    // It is needed to avoid going crazy over quoting.
-    let action_dump = r#"["CreateGeneratorZero",{"Select":0},{"SetBoundary":"source"},{"Select":0},{"SetBoundary":"target"},{"SelectGenerator":{"dimension":1,"id":1}},{"SelectPoints":[["target"]]},{"SetBoundary":"source"},{"SelectGenerator":{"dimension":1,"id":1}},{"SetBoundary":"target"},{"SelectGenerator":{"dimension":2,"id":2}},{"SelectPoints":[["source",1]]},{"SetBoundary":"source"},{"SelectGenerator":{"dimension":2,"id":2}},{"SelectPoints":[["source",3]]},{"SetBoundary":"target"},{"SelectGenerator":{"dimension":3,"id":3}}]"#;
-
-    let actions: Vec<Action> = serde_json::from_str(&action_dump).unwrap();
+fn actions_test_helper(json: &str) -> Proof {
+    let actions: Vec<Action> = serde_json::from_str(json).unwrap();
     let mut proof: Proof = Default::default();
 
     for a in actions.iter() {
@@ -23,7 +17,27 @@ fn construct_associator() {
         }
     }
 
+    proof
+}
+
+#[test]
+fn construct_associator() {
+    let action_dump = include_str!("examples/associator.json");
+
+    let proof = actions_test_helper(&action_dump);
+
     // Snapshot the end result
     assert_debug_snapshot!("construct_associator_signature", proof.signature);
     assert_debug_snapshot!("construct_associator_workspace", proof.workspace);
+}
+
+#[test]
+fn construct_braid_and_contract() {
+    let action_dump = include_str!("examples/braid_contraction.json");
+
+    let proof = actions_test_helper(&action_dump);
+
+    // Snapshot the end result
+    assert_debug_snapshot!("construct_braid_contraction_signature", proof.signature);
+    assert_debug_snapshot!("construct_braid_contraction_workspace", proof.workspace);
 }
