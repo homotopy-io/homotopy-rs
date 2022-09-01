fd7fc09e85818f3b158659a81dbc6aaa6e45ce59
regular-citizen <7887982+regular-citizen@users.noreply.github.com>
2022-09-01 20:37:47 +0100

    Panic display panel now hidden, can be summoned from console.

--- homotopy-web/src/app/sidebar.rs
+++ homotopy-web/src/app/sidebar.rs
@@ -344,7 +344,7 @@ impl Component for Sidebar {
         html! {
             <>
                 <aside class="sidebar">
-                    <a href="#panic">
+                    <a href="#about">
                         <img src="/logo.svg" alt="Homotopy.io logo" class="sidebar__logo" />
                     </a>
                     {self.nav(ctx)}
