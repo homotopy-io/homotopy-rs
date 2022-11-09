1647e11f9305fd6fa26b8106e395524013d2f746
Nick Hu <me@nickhu.co.uk>
2022-11-09 11:32:40 +0000

    Add collapse test for weak identity on braid

--- homotopy-core/src/label.rs
+++ homotopy-core/src/label.rs
@@ -1,7 +1,8 @@
 use std::collections::BTreeSet;
 
-use crate::{common::BoundaryPath, Generator, Height};
+use crate::{collapse::OneMany, common::BoundaryPath, Generator, Height};
 
 pub(crate) type Coord = Vec<Height>;
+pub(crate) type Coords = OneMany<Coord, BTreeSet<Coord>>;
 
 pub(crate) type Label = Option<(Generator, BoundaryPath, BTreeSet<Coord>)>;
