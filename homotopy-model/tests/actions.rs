1abbad3a2210877f38d196ac0f275d47b47efe84
regular-citizen <7887982+regular-citizen@users.noreply.github.com>
2022-12-10 21:02:52 +0000

    Added braiding half bubbles test case (against #947 regression).

--- homotopy-model/tests/actions.rs
+++ homotopy-model/tests/actions.rs
@@ -1,8 +1,7 @@
-use homotopy_core::typecheck::{typecheck, Mode};
 pub use homotopy_model::{history::Proof, proof::Action, serialize};
 use insta::assert_debug_snapshot;
 
-fn actions_test_helper(json: &str, should_check: bool) -> Proof {
+fn actions_test_helper(json: &str) -> Proof {
     let (_safe, actions): (bool, Vec<Action>) = serde_json::from_str(json).unwrap();
     let mut proof: Proof = Default::default();
 
@@ -10,13 +9,6 @@ fn actions_test_helper(json: &str, should_check: bool) -> Proof {
         proof
             .update(a)
             .expect("Actions should replay without errors.");
-        // Typecheck as we go along
-        if should_check {
-            if let Some(workspace) = &proof.workspace {
-                typecheck(&workspace.diagram, &proof.signature, Mode::Deep)
-                    .unwrap_or_else(|_| panic!("Typechecking failure at action: {:?}.", a));
-            }
-        }
     }
 
     proof
@@ -26,7 +18,7 @@ fn actions_test_helper(json: &str, should_check: bool) -> Proof {
 fn construct_associator() {
     let action_dump = include_str!("examples/associator.json");
 
-    let proof = actions_test_helper(action_dump, true);
+    let proof = actions_test_helper(action_dump);
 
     // Snapshot the end result
     assert_debug_snapshot!("construct_associator_signature", proof.signature);
@@ -34,15 +26,20 @@ fn construct_associator() {
 }
 
 #[test]
+fn construct_braiding_half_bubbles() {
+    let action_dump = include_str!("examples/braiding_half_bubbles.json");
+
+    let proof = actions_test_helper(action_dump);
+
+    // Snapshot the end result
+    assert_debug_snapshot!("construct_braiding_half_bubbles_signature", proof.signature);
+    assert_debug_snapshot!("construct_braiding_half_bubbles_workspace", proof.workspace);
+}
+
+#[test]
 fn construct_adjoint_equivalence() {
     let action_dump = include_str!("examples/equivalence_to_adjoint_equivalence.json");
 
-    let proof = actions_test_helper(action_dump, false);
+    let _proof = actions_test_helper(action_dump);
     // Snapshotting with debug takes too long
-    typecheck(
-        &proof.workspace.as_ref().unwrap().diagram,
-        &proof.signature,
-        Mode::Deep,
-    )
-    .expect("Typecheck failure.");
 }
