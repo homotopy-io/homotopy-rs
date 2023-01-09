84af96ea9827cec40187632d820b6bfeda93938a
regular-citizen <7887982+regular-citizen@users.noreply.github.com>
2023-01-09 11:10:01 +0000

    Removed snapshotting from action tests (#967).

--- homotopy-model/tests/actions.rs
+++ homotopy-model/tests/actions.rs
@@ -1,5 +1,4 @@
 pub use homotopy_model::{history::Proof, proof::Action, serialize};
-use insta::assert_debug_snapshot;
 
 fn actions_test_helper(json: &str) -> Proof {
     let (_safe, actions): (bool, Vec<Action>) = serde_json::from_str(json).unwrap();
@@ -18,22 +17,14 @@ fn actions_test_helper(json: &str) -> Proof {
 fn construct_associator() {
     let action_dump = include_str!("examples/associator.json");
 
-    let proof = actions_test_helper(action_dump);
-
-    // Snapshot the end result
-    assert_debug_snapshot!("construct_associator_signature", proof.signature);
-    assert_debug_snapshot!("construct_associator_workspace", proof.workspace);
+    let _proof = actions_test_helper(action_dump);
 }
 
 #[test]
 fn construct_braiding_half_bubbles() {
     let action_dump = include_str!("examples/braiding_half_bubbles.json");
 
-    let proof = actions_test_helper(action_dump);
-
-    // Snapshot the end result
-    assert_debug_snapshot!("construct_braiding_half_bubbles_signature", proof.signature);
-    assert_debug_snapshot!("construct_braiding_half_bubbles_workspace", proof.workspace);
+    let _proof = actions_test_helper(action_dump);
 }
 
 #[test]
@@ -41,5 +32,4 @@ fn construct_adjoint_equivalence() {
     let action_dump = include_str!("examples/equivalence_to_adjoint_equivalence.json");
 
     let _proof = actions_test_helper(action_dump);
-    // Snapshotting with debug takes too long
 }
