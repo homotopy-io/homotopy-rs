f0f95a2cf3369f0c39f4d16899b30d48173d695a
Chiara Sarti <7887982+regular-citizen@users.noreply.github.com>
2023-03-13 11:43:19 +0000

    CSS changes (#1041)
    * Minor CSS setup changes.
    
    * Minor sidebar CSS changes.
    
    * More sidebar CSS changes.
    
    * Fixed font sizes and workspace zooms.
    
    * Fixed modal CSS.
    
    * Fixed panzoom problem.
    
    * Minor signature edit.
    
    * Removed dead code.
    
    * Removed dead code.

--- homotopy-web/src/app/sidebar.rs
+++ homotopy-web/src/app/sidebar.rs
@@ -233,9 +233,7 @@ impl Component for SidebarDrawer {
                 >
                 <div class="drawer__inner">
                     <div class="drawer__header">
-                        <span class="drawer__title">
-                            {ctx.props().title}
-                        </span>
+                        <h3 class="drawer__title">{ctx.props().title}</h3>
                         if let (Some(icon), Some(action)) = (ctx.props().icon, ctx.props().on_click.as_ref().cloned()) {
                             <span
                                 class="drawer__icon"
