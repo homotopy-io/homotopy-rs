cd35d7d836f8788f3257434b32a428914a746c8a
calintat <calintat@gmail.com>
2023-11-08 14:31:48 +0000

    Deploying to gh-pages from @ homotopy-io/homotopy-rs@7907bbee8859ad2595d72334b8242175b656e078 🚀

--- homotopy_web.js
+++ homotopy_web.js
@@ -1392,11 +1392,11 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 476, __wbg_adapter_45);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5070 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper5117 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1900, __wbg_adapter_48);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5304 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper5351 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1931, __wbg_adapter_51);
         return addHeapObject(ret);
     };
